de3f1af6713135faf9aec2de960850f02d52af12
romanlenger <145775490+romanlenger@users.noreply.github.com>
2023-12-26 11:45:48 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='sort-folder-script',
     version='0.0.2',
     author='Roman Kripak',
-    url='https://github.com/romanlenger/homeworkTask7_Kripak',
+    url='https://github.com/romanlenger/homeworkTask7_Kripak/tree/romanlenger-patch-1',
     packages=find_packages(),
     entry_points={
         'console_scripts': [

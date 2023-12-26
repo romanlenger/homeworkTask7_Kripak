b04e83bd016e737b43610b254a84c1e1dab1909d
romanlenger <145775490+romanlenger@users.noreply.github.com>
2023-12-26 11:27:37 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'clean-folder = clean_folder.clean_folder.clean:main',
+            'clean-folder = clean_folder.clean:main',
         ]
     }
 )
